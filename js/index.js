578d9426138738e7f6bb685e7743a9f5af397e83
Evan Tseng <evan@tseng.io>
2016-09-18 23:20:07 +0800

    Reload MJPEG stream when it is disconnected

--- js/index.js
+++ js/index.js
@@ -2,6 +2,24 @@
 
 (function() {
   var params = new URLSearchParams(window.location.search);
+  var url = params.get('url');
   var viewer = document.querySelector('#viewer');
-  viewer.setAttribute('src', params.get('url'));
+
+  loadMjpeg();
+
+  function loadMjpeg() {
+    viewer.setAttribute('src', url);
+    // Workaround to reload MJPEG stream when it's disconnected.
+    var ajax = new XMLHttpRequest();
+    ajax.open('GET', url, true);
+    ajax.responseType = 'arraybuffer';
+    ajax.onload = function() {
+      alert('load');
+      setTimeout(loadMjpeg, 5000);
+    };
+    ajax.onerror = function() {
+      setTimeout(loadMjpeg, 5000);
+    };
+    ajax.send();
+  }
 }());
