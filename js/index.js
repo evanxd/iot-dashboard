9044af2a80b3ece5c6021af67f1d0d9af3b843b2
Evan Tseng <evan@tseng.io>
2017-01-15 20:33:30 +0800

    Reload image when the steam is failed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function() {
+  var CHECK_INTERVAL = 10000;
   var urls = new Url(window.location.search).query.urls;
   if (!urls) {
     alert('Cannot get urls param.');
@@ -13,11 +14,22 @@
   urls.forEach(function(url, i) {
     var image = new Image();
     var id = 'mjpeg-' + (i + 1);
+    var isImageActive = false;
+
+    image.onload = function() {
+      isImageActive = true;
+    };
+    setInterval(function() {
+      !isImageActive && image.setAttribute('src', url);
+      isImageActive = false;
+    }, CHECK_INTERVAL);
+
     image.onerror = function() {
       setTimeout(function() {
         image.setAttribute('src', url);
-      }, 10000);
+      }, CHECK_INTERVAL);
     };
+
     image.setAttribute('id', id);
     image.setAttribute('src', url);
     viewer.appendChild(image);
