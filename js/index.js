8d5607c04e4f0563ac38c67bdecad15a302b81e3
Evan Tseng <evan@tseng.io>
2017-03-18 21:44:16 +0800

    Support iframe element

--- js/index.js
+++ js/index.js
@@ -12,26 +12,37 @@
   var viewer = document.querySelector('#viewer');
 
   urls.forEach(function(url, i) {
-    var image = new Image();
     var id = 'mjpeg-' + (i + 1);
-    var isImageActive = false;
+    // Workaround to append image and iframe elements.
+    // Need to figure out how to detect the mime type to fix it.
+    if (i < 2) {
+      var image = new Image();
+      var isImageActive = false;
 
-    image.onload = function() {
-      isImageActive = true;
-    };
-    setInterval(function() {
-      !isImageActive && image.setAttribute('src', url);
-      isImageActive = false;
-    }, CHECK_INTERVAL);
-
-    image.onerror = function() {
-      setTimeout(function() {
-        image.setAttribute('src', url);
+      image.onload = function() {
+        isImageActive = true;
+      };
+      setInterval(function() {
+        !isImageActive && image.setAttribute('src', url);
+        isImageActive = false;
       }, CHECK_INTERVAL);
-    };
 
-    image.setAttribute('id', id);
-    image.setAttribute('src', url);
-    viewer.appendChild(image);
+      image.onerror = function() {
+        setTimeout(function() {
+          image.setAttribute('src', url);
+        }, CHECK_INTERVAL);
+      };
+
+      image.setAttribute('id', id);
+      image.setAttribute('src', url);
+      viewer.appendChild(image);
+    } else {
+      var iframe = document.createElement('iframe');
+      iframe.setAttribute('id', id);
+      iframe.setAttribute('src', url);
+      iframe.setAttribute('frameborder', 0);
+      iframe.setAttribute('scrolling', 'no');
+      viewer.appendChild(iframe);
+    }
   });
 }());
